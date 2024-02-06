e324254766cfdcd4aa12f6ae9e548efaceff5789
AldoRambaut <rambaut.0512@gmail.com>
2024-02-05 22:28:53 -0500

    Correccion de los ejercicios que tenian js

--- ejercicio9/app.js
+++
@@ -1,9 +0,0 @@
-const borde = document.getElementById(`caja`);
-
-borde.addEventListener('mousemove', (e) => {
-    console.log(e);
-    const x = e.clientX - borde.offsetLeft;
-    const y = e.clientY - borde.offsetTop;
-    console.log(e.clientX - borde.offsetLeft, e.clientY - borde.offsetTop);
-    borde.style.opacity= 1- (x + y) / 600;
-})
